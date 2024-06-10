569974350ef8421f609a8e5f181b75d9310d5030
filbertfelim <76082420+filbertfelim@users.noreply.github.com>
2024-06-10 23:24:57 +0700

    ui : add movie card swipe behavior and update navbar icon

--- app/src/main/java/com/example/cinematch/ui/theme/nav/NavItem.kt
+++ app/src/main/java/com/example/cinematch/ui/theme/nav/NavItem.kt
@@ -1,20 +1,20 @@
 package com.example.cinematch.ui.theme.nav
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.PlaylistAdd
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
 
 sealed class NavItem {
     object Home :
         Item(path = NavPath.HOME.toString(), title = NavTitle.HOME, icon = Icons.Default.Home)
 
     object Watchlist :
-        Item(path = NavPath.WATCHLIST.toString(), title = NavTitle.WATCHLIST, icon = Icons.Default.Search)
+        Item(path = NavPath.WATCHLIST.toString(), title = NavTitle.WATCHLIST, icon = Icons.AutoMirrored.Filled.PlaylistAdd)
 
     object Watched :
-        Item(path = NavPath.WATCHED.toString(), title = NavTitle.WATCHED, icon = Icons.Default.List)
+        Item(path = NavPath.WATCHED.toString(), title = NavTitle.WATCHED, icon = Icons.Default.CheckCircle)
 
     object Profile :
         Item(
--- app/src/main/java/com/example/cinematch/ui/theme/screen/HomeScreen.kt
+++ app/src/main/java/com/example/cinematch/ui/theme/screen/HomeScreen.kt
@@ -1,40 +1,126 @@
 package com.example.cinematch.ui.theme.screen
 
+import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.cinematch.R
+import com.alexstyl.swipeablecard.Direction
+import com.alexstyl.swipeablecard.ExperimentalSwipeableCardApi
+import com.alexstyl.swipeablecard.rememberSwipeableCardState
+import com.alexstyl.swipeablecard.swipableCard
 
+data class Movie(val id: Int, val title: String, val poster: Int)
+
+@OptIn(ExperimentalSwipeableCardApi::class)
 @Composable
 fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
+    val movies = listOf(
+        Movie(1, "Movie 1", R.drawable.jedi),
+        Movie(2, "Movie 2", R.drawable.jedi),
+        Movie(3, "Movie 3", R.drawable.jedi)
+    )
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Box(
+        modifier = Modifier
             .fillMaxSize()
             .background(Color(0xff121212))
-            .padding(16.dp)
+            .systemBarsPadding()
     ) {
-        Text(
-            text = "HOME",
-            color = Color(0xfff3f3f3),
-            textAlign = TextAlign.Center,
-            style = TextStyle(
-                fontSize = 24.sp, fontWeight = FontWeight.Bold
-            ),
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
+        Box {
+            val states = movies.reversed()
+                .map { it to rememberSwipeableCardState() }
+            val scope = rememberCoroutineScope()
+            Box(
+                Modifier
+                    .padding(24.dp)
+                    .fillMaxSize()
+                    .aspectRatio(1f)
+                    .align(Alignment.Center)
+            ) {
+                states.forEach { (movie, state) ->
+                    if (state.swipedDirection == null) {
+                        MovieCard(
+                            title = movie.title,
+                            posterResId = movie.poster,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .swipableCard(
+                                    state = state,
+                                    blockedDirections = listOf(Direction.Down),
+                                    onSwiped = {
+                                        // handle di LaunchedEffect
+                                    },
+                                    onSwipeCancel = {
+                                        Log.d("Swipeable-Card", "Cancelled swipe")
+                                    }
+                                ),
+                        )
+
+                    }
+                    LaunchedEffect(movie, state.swipedDirection) {
+                        if (state.swipedDirection != null) {
+
+                        }
+                        //  TODO : FETCH API BERDASARKAN SWIPE DIRECTION
+                    }
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun MovieCard(
+    title: String,
+    posterResId: Int,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier.padding(8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFF121212))
+    ) {
+        Box {
+            Image(
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(posterResId),
+                contentDescription = null
+            )
+            Scrim(Modifier.align(Alignment.BottomCenter))
+            Column(Modifier.align(Alignment.BottomStart)) {
+                Text(
+                    text = title,
+                    color = Color.White,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Scrim(modifier: Modifier = Modifier) {
+    Box(
+        modifier
+            .background(Brush.verticalGradient(listOf(Color.Transparent, Color.Black)))
+            .height(180.dp)
+            .fillMaxWidth()
+    )
 }
